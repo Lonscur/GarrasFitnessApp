2fa20fac6905bd3ca3e309a6962c05d164499b5a
MRCC-NIX <bylenixroyale63@gmail.com>
2026-09-21 02:02:37 -0400

    caja

---
+++ GarrasFitnessApp/Controllers/CajaController.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using GarrasFitnessApp.Models; // Ya no lleva .ViewModels al final
+
+namespace GarrasFitnessApp.Controllers
+{
+    public class CajaController : Controller
+    {
+        public IActionResult Index()
+        {
+            var mockData = new ArqueoCajaViewModel();
+            return View(mockData);
+        }
+
+        public IActionResult ExportarPDF()
+        {
+            return Content("El reporte PDF se generará aquí.");
+        }
+    }
+}
---
+++ GarrasFitnessApp/Models/ArqueoCajaViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace GarrasFitnessApp.Models
+{
+    public class ArqueoCajaViewModel
+    {
+        public decimal TotalEfectivo { get; set; }
+        public decimal TotalQR { get; set; }
+        public decimal TotalGeneral => TotalEfectivo + TotalQR;
+        public DateTime FechaConsulta { get; set; } = DateTime.Now;
+    }
+}
